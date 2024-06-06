4486be1a6912a98a36c5fd6dbf51aba823b70398
Mohd Mustajab <mohdmustajab002@gmail.com>
2024-06-06 11:06:33 +0530

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './Navbar';
 import Users from './Users';
 
 function App() {
 
   return (
     <div>
+      <Navbar/>
     <Users />
   </div>
 );
---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav>
+       <div>Users Data</div>
+    </nav>
+  )
+}
+
+export default Navbar
